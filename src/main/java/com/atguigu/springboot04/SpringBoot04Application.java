339c46d421851403e9042747d94c148b4f19d7ed
158likun <1976391536@qq.com>
2020-03-06 21:41:48 +0800

    '二手交易'

---
+++ src/main/java/com/atguigu/springboot04/SpringBoot04Application.java
@@ -0,0 +1,14 @@
+package com.atguigu.springboot04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class SpringBoot04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot04Application.class, args);
+	}
+
+}
